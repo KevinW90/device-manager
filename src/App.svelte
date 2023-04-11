0b36b73a6d948de83cb5c420da342d351a08abee
KevinW90 <kevinjwilliams76@gmail.com>
2023-04-11 15:57:04 -0400

    greeting section content and style

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,23 @@
 <script lang="ts">
+  import Greeting from "./lib/Greeting.svelte";
+
 </script>
 
 <main>
-  
+  <Greeting />
 </main>
 
-<style>
+<style lang="scss">
+  $base: hsl(227, 42%, 33%);
+
+  :global(html, body) {
+    margin: 0;
+    padding: 0;
+    font-family: sans-serif;
+  }
+  
+  main {
+    height: 100vh;
+    background-color: $base;
+  }
 </style>
---
+++ src/lib/Greeting.svelte
@@ -0,0 +1,52 @@
+<section id="greeting">
+  <div class="introduction">
+    <div class="hello">Hello,</div>
+    <div class="name">Grumpy Wizard</div>
+
+  </div>
+  <div class="profile">
+    <img src="https://placekitten.com/50/50" alt="Kitten" />
+  </div>
+</section>
+
+<style lang="scss">
+  #greeting {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    padding: 4rem 1rem 0 1rem;
+  }
+
+  .introduction {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: flex-start;
+  }
+
+  .hello {
+    font-size: 1.5rem;
+    font-weight: 600;
+    color: rgba(white, 0.5);
+  }
+
+  .name {
+    font-size: 1.5rem;
+    font-weight: 700;
+    color: white;
+  }
+
+  .profile {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .profile img {
+    width: 50px;
+    height: 50px;
+    border-radius: .5rem;
+  }
+</style>
